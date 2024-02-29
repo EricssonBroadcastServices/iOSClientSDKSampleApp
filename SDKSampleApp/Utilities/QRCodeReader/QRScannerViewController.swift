7fe989dfa7377c7331e5009461d476b54dc24c3a
Radosław Tarnawski <r.tarnawski@bsgroup.eu>
2024-02-29 16:27:41 +0100

    EMP-21046 - Struct holding encoded QR code parameters

--- SDKSampleApp/Utilities/QRCodeReader/QRScannerViewController.swift
+++ SDKSampleApp/Utilities/QRCodeReader/QRScannerViewController.swift
@@ -8,8 +8,6 @@ class QRScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsD
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.navigationItem.title = NSLocalizedString("QR Reader", comment: "")
 
         session = AVCaptureSession()
 
@@ -40,12 +38,24 @@ class QRScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsD
         previewLayer.videoGravity = .resizeAspectFill
         view.layer.addSublayer(previewLayer)
 
-        // Add transparent square view as an overlay to indicate the smaller square window
-        let overlayView = UIView(frame: rectOfInterest)
-        overlayView.layer.borderWidth = 2
-        overlayView.layer.borderColor = UIColor.red.cgColor
-        overlayView.backgroundColor = UIColor.clear
+        // Add semi-transparent overlay view to dim the area outside the red square
+        let overlayView = UIView(frame: view.bounds)
+        overlayView.backgroundColor = UIColor.black.withAlphaComponent(0.5)
         view.addSubview(overlayView)
+
+        // Add the transparent square view as an overlay to indicate the smaller square window
+        let squareView = UIView(frame: rectOfInterest)
+        squareView.layer.borderWidth = 2
+        squareView.layer.borderColor = UIColor.red.cgColor
+        squareView.backgroundColor = UIColor.clear
+        view.addSubview(squareView)
+
+        // Cut out the region corresponding to the red square from the overlay view
+        let maskLayer = CAShapeLayer()
+        let path = UIBezierPath(rect: overlayView.bounds)
+        path.append(UIBezierPath(rect: squareView.frame).reversing())
+        maskLayer.path = path.cgPath
+        overlayView.layer.mask = maskLayer
 
         session.startRunning()
     }
@@ -56,7 +66,10 @@ class QRScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsD
             if let metadataObject = metadataObjects.first as? AVMetadataMachineReadableCodeObject {
                 if metadataObject.type == .qr {
                     if let qrCodeData = metadataObject.stringValue {
-                        print("QR Code Data: \(qrCodeData)")
+//                        print("QR Code Data: \(qrCodeData)")
+                        
+                        let parameters = extractURLParameters(from: qrCodeData)
+                        print(parameters)
                         // Handle the QR code data as needed
                         // Stop scanning more QR codes
                         session.stopRunning()
@@ -70,8 +83,6 @@ class QRScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsD
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        self.navigationController?.setNavigationBarHidden(false, animated: false)
 
         if session?.isRunning == false {
             session.startRunning()
@@ -85,5 +96,60 @@ class QRScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsD
         if session?.isRunning == true {
             session.stopRunning()
         }
+    }
+    
+    func extractURLParameters(from qrCodeData: String) -> QRCodeURLParameters {
+        var parameters = QRCodeURLParameters()
+        
+        // Remove the leading prefix if present
+        let cleanedData = qrCodeData.replacingOccurrences(of: "https://ericssonbroadcastservices.github.io/javascript-player/?", with: "")
+        
+        // Split the string by "&" to get individual parameters
+        let parameterPairs = cleanedData.components(separatedBy: "&")
+        
+        // Iterate over parameter pairs and extract key-value pairs
+        for pair in parameterPairs {
+            let components = pair.components(separatedBy: "=")
+            if components.count == 2 {
+                if let key = components[0].removingPercentEncoding, let value = components[1].removingPercentEncoding {
+                    if let parameter = QRCodeParameter(rawValue: key) {
+                        switch parameter {
+                        case .env: parameters.env = value
+                        case .cu: parameters.cu = value
+                        case .bu: parameters.bu = value
+                        case .source: parameters.source = value
+                        case .sessionToken: parameters.sessionToken = value
+                        }
+                    }
+                }
+            }
+        }
+        
+        return parameters
+    }
+
+}
+
+enum QRCodeParameter: String {
+    case env
+    case cu
+    case bu
+    case source
+    case sessionToken
+}
+
+struct QRCodeURLParameters {
+    var env: String?
+    var cu: String?
+    var bu: String?
+    var source: String?
+    var sessionToken: String?
+    
+    init(env: String? = nil, cu: String? = nil, bu: String? = nil, source: String? = nil, sessionToken: String? = nil) {
+        self.env = env
+        self.cu = cu
+        self.bu = bu
+        self.source = source
+        self.sessionToken = sessionToken
     }
 }
